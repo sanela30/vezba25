246b4ced64eae32ef77bccea391e31c4b76d00d1
Sanela Cvijic <sanela26@gmail.com>
2022-03-09 12:49:56 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,49 @@ namespace vezba25
     {
         static void Main(string[] args)
         {
+            string choice;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("QA telefonski imenik \n");
+                Console.WriteLine("1.Unos novog imena");
+                Console.WriteLine("2.Unos novog imena");
+                Console.WriteLine("3.Pretraga");
+                Console.WriteLine("Q.Kraj rada");
+                choice = Console.ReadLine();
+
+
+                if (choice == "1")
+                {
+                    AddNewName();
+                }
+
+            } while (choice.ToUpper() != "Q");
+
+        }
+
+        static void AddNewName()
+        {
+            string firstName, lastName, address, phone, choice;
+
+            do
+            {
+                Console.Write("Unseite ime>");
+                firstName = Console.ReadLine();
+                Console.Write("Unseite prezime>");
+                lastName = Console.ReadLine();
+                Console.Write("Unseite adresu>");
+                address = Console.ReadLine();
+                Console.Write("Unseite telefon>");
+                phone = Console.ReadLine();
+
+                Console.WriteLine("\n Da li zelite unos novog imena?Ukoliko zelite otkucajte slovo 'Y',ili ENTER ukoliko zelite prekinete");
+                choice = Console.ReadLine();
+                Console.WriteLine("");
+
+
+            } while (choice.ToUpper() == "Yes");
         }
     }
 }
